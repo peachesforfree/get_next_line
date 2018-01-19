d68dcaf8830ea4324730f37cf36a30a61267eb23
Sergio BALCORTA <sbalcort@e1z3r7p24.42.us.org>
2018-01-18 17:40:49 -0800

    get_next_line

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbalcort <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/30 22:00:54 by sbalcort          #+#    #+#             */
+/*   Updated: 2017/01/30 22:23:02 by sbalcort         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include "libft/libft.h"
+# include <stdlib.h>
+# include <unistd.h>
+
+# define BUFF_SIZE 64
+
+int		get_next_line(const int fd, char **line);
+#endif
